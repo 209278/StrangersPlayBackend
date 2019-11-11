b2d5073a90d72cb4ba3d26e8228c7b4b93ba9dcc
Zandros229 <ernest.figura548@gmail.com>
2019-11-11 18:01:17 +0100

    fix date format

--- src/main/java/com/spb/StrangersPlayBackend/StrangersPlayBackendApplication.java
+++ src/main/java/com/spb/StrangersPlayBackend/StrangersPlayBackendApplication.java
@@ -16,22 +16,22 @@ import java.time.Instant;
 public class StrangersPlayBackendApplication {
 
 	public static void main(String[] args) {
-		AdvertisementDto accountDto = new AdvertisementDto();
-		accountDto.setCategory(Category.PILKA_NOZNA);
-		accountDto.setLevel(1);
-		accountDto.setPrice(10.0);
-		accountDto.setCreationTime(Instant.now());
-		accountDto.setUsername("janusz");
-		accountDto.setEventTime("2019-11-11T15:52:50.065Z");
-		accountDto.setTitle("granie w piłkę");
-		accountDto.setDescription("granie w piłke za 3 DS PŁ");
-		accountDto.setEventLocation("3 ds PŁ");
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			System.out.println(mapper.writeValueAsString(accountDto));
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
+//		AdvertisementDto accountDto = new AdvertisementDto();
+//		accountDto.setCategory(Category.PILKA_NOZNA);
+//		accountDto.setLevel(1);
+//		accountDto.setPrice(10.0);
+//		accountDto.setCreationTime(Instant.now());
+//		accountDto.setUsername("janusz");
+//		accountDto.setEventTime("2019-11-11T15:52:50.065Z");
+//		accountDto.setTitle("granie w piłkę");
+//		accountDto.setDescription("granie w piłke za 3 DS PŁ");
+//		accountDto.setEventLocation("3 ds PŁ");
+//		ObjectMapper mapper = new ObjectMapper();
+//		try {
+//			System.out.println(mapper.writeValueAsString(accountDto));
+//		} catch (JsonProcessingException e) {
+//			e.printStackTrace();
+//		}
 
 		SpringApplication.run(StrangersPlayBackendApplication.class, args);
 	}
