ff08cf39e9f088ce158f213e0f420b72dd06f87b
209420 <209420@edu.p.lodz.pl>
2019-11-13 21:38:44 +0100

    category image fix

--- src/main/java/com/spb/StrangersPlayBackend/model/Category.java
+++ src/main/java/com/spb/StrangersPlayBackend/model/Category.java
@@ -1,20 +1,20 @@
 package com.spb.StrangersPlayBackend.model;
 
 public enum Category {
-    PILKA_NOZNA("Piłka nożna","/categoryImages/football.png"),
-    KRYKIET("Krykiet", "/categoryImages/other.png"),
-    KOSZYKOWKA("Koszykówka", "/categoryImages/basketball.png"),
-    HOKEJ("Hokej", "/categoryImages/other.png"),
-    TENIS_ZIEMNY("Tenis ziemny", "/categoryImages/tennis.png"),
-    SIATKOWKA("Siatkówka", "/categoryImages/volleyball.png"),
-    TENIS_STOLOWY("Tenis stołowy", "/categoryImages/pingpong.png"),
-    BASEBALL("Baseball", "/categoryImages/other.png"),
-    RUGBY("Rugby", "/categoryImages/rugby.png"),
-    GOLF("Golf", "/categoryImages/other.png"),
-    BIEGANIE("Bieganie", "/categoryImages/running.png"),
-    KOLARSTWO("Kolarstwo", "/categoryImages/cycling.png"),
-    PILKA_RECZNA("Piłka ręczna", "/categoryImages/other.png"),
-    SILOWNIA("Siłownia", "/categoryImages/gym.png");
+    PILKA_NOZNA("Piłka nożna","categoryImages/football.png"),
+    KRYKIET("Krykiet", "categoryImages/other.png"),
+    KOSZYKOWKA("Koszykówka", "categoryImages/basketball.png"),
+    HOKEJ("Hokej", "categoryImages/other.png"),
+    TENIS_ZIEMNY("Tenis ziemny", "categoryImages/tennis.png"),
+    SIATKOWKA("Siatkówka", "categoryImages/volleyball.png"),
+    TENIS_STOLOWY("Tenis stołowy", "categoryImages/pingpong.png"),
+    BASEBALL("Baseball", "categoryImages/other.png"),
+    RUGBY("Rugby", "categoryImages/rugby.png"),
+    GOLF("Golf", "categoryImages/other.png"),
+    BIEGANIE("Bieganie", "categoryImages/running.png"),
+    KOLARSTWO("Kolarstwo", "categoryImages/cycling.png"),
+    PILKA_RECZNA("Piłka ręczna", "categoryImages/other.png"),
+    SILOWNIA("Siłownia", "categoryImages/gym.png");
 
     private final String categoryName;
     private final String filename;
