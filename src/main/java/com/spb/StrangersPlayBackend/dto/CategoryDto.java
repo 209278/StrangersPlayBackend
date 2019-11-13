144adb74c4005f4f6801f5d469406162fb2c46e7
209420 <209420@edu.p.lodz.pl>
2019-11-13 14:26:30 +0100

    added images to advertisement and category. Login response now is returning userId

--- src/main/java/com/spb/StrangersPlayBackend/dto/CategoryDto.java
+++ src/main/java/com/spb/StrangersPlayBackend/dto/CategoryDto.java
@@ -3,15 +3,23 @@ package com.spb.StrangersPlayBackend.dto;
 import com.spb.StrangersPlayBackend.model.Category;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
 
 @Getter
 @Setter
 public class CategoryDto {
     private String type;
     private String name;
+    private String image;
 
-    public CategoryDto(Category category) {
+    public CategoryDto(Category category) throws IOException {
         this.name = category.getCategoryName();
         this.type = category.name();
+        this.image = Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(new File(getClass().getResource(category.getFilename()).getPath())));
     }
 }
