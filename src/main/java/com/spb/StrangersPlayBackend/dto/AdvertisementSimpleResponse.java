2ee2315edb8851c827a2eb6fdde79b4ca0dbe3a5
209420 <209420@edu.p.lodz.pl>
2019-11-24 22:45:53 +0100

    simpleResponseAdvertisement improvement

--- src/main/java/com/spb/StrangersPlayBackend/dto/AdvertisementSimpleResponse.java
+++ src/main/java/com/spb/StrangersPlayBackend/dto/AdvertisementSimpleResponse.java
@@ -1,11 +1,13 @@
 package com.spb.StrangersPlayBackend.dto;
 
 import com.spb.StrangersPlayBackend.model.Category;
+import com.spb.StrangersPlayBackend.model.UserInAdvertisementModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
@@ -20,6 +22,7 @@ public class AdvertisementSimpleResponse {
     private int authorId;
     private int userLimit;
     private int usersJoined;
+    private List<UserInAdvertisementModel> userIdsList;
     private String eventTime;
     private String eventLocation;
     private Instant creationTime;
